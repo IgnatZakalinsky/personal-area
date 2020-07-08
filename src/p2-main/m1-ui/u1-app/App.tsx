9dbe27163f86add5b15f7857223fe3cd8ae6bc88
ai73aaa <1qazxcvBG>
2020-07-08 21:18:00 +0300

    add antd

--- src/p2-main/m1-ui/u1-app/App.tsx
+++ src/p2-main/m1-ui/u1-app/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import s from "./App.module.css";
+import 'antd/dist/antd.css';
 import {log} from "../../../p1-common/c0-debug/debug";
+import {Button} from "antd";
 
 const App = () => {
 
     log("1 - rendering App");
     return (
         <div className={s.App}>
+            <Button>test</Button>
 
 
             it-incubator
