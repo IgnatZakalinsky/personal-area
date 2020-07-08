40dff7168fd35487b255d645faa8b48ce5d4d184
ai73aaa <1qazxcvBG>
2020-07-08 21:47:32 +0300

    add Containers

---
+++ src/p1-common/c1-ui/u1-containers/Container.tsx
@@ -0,0 +1,28 @@
+import React, {DetailedHTMLProps, HTMLAttributes} from "react";
+import {log} from "../../c0-debug/debug";
+
+type DivPropsType = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+type ContainerPropsType = DivPropsType & {
+    direction?: "column" | "row"
+
+    renderLog?: string
+}
+
+const Container: React.FC<ContainerPropsType> = React.memo((
+    {
+        direction = "column",
+        style,
+
+        renderLog,
+        ...restProps
+    }
+) => {
+
+    log(renderLog || `rendering container type ${direction}`);
+    return (
+        <div style={{display: "flex", flexFlow: direction, ...style}} {...restProps}/>
+    );
+});
+
+export default Container;
--- src/p2-main/m1-ui/u1-app/App.tsx
+++ src/p2-main/m1-ui/u1-app/App.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import s from "./App.module.css";
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'; // antd
 import {log} from "../../../p1-common/c0-debug/debug";
 import {Button} from "antd";
+import Container from "../../../p1-common/c1-ui/u1-containers/Container";
 
-const App = () => {
+const App = React.memo(() => {
+    const [test, setTest] = useState<string>('x');
+    const onClick = useCallback(() => setTest('test'), [setTest]);
+    const [test2, setTest2] = useState<string>('x2');
+    const onClick2 = useCallback(() => setTest2('test2'), [setTest2]);
 
     log("1 - rendering App");
     return (
         <div className={s.App}>
-            <Button>test</Button>
+            <Container renderLog={"2 -- rendering column app div"}>
+                <Button onClick={onClick}>{test}</Button>
+                <Button onClick={onClick2}>{test2}</Button>
 
 
-            it-incubator
+                it-incubator
+            </Container>
         </div>
     );
-};
+});
 
 export default App;
