16355c722990fd82f577fda1f9113d381c53f1c3
ai73aaa <1qazxcvBG>
2020-07-15 17:09:17 +0300

    add onChangeText in CustomInput

--- src/p1-common/c1-ui/u2-inputs/CustomInput.tsx
+++ src/p1-common/c1-ui/u2-inputs/CustomInput.tsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, {ChangeEvent, useCallback} from "react";
 import {log} from "../../c0-debug/debug";
 import {Input} from "antd";
 import {InputProps} from "antd/es/input";
 import s from "./CustomInput.module.css";
 
 type CustomInputPropsType = InputProps & {
+    onChangeText?: (value: string) => void
 
     renderLog?: string
 }
 
 const CustomInput: React.FC<CustomInputPropsType> = React.memo((
     {
+        onChange,
+        onChangeText,
         className,
 
         renderLog,
         ...restProps
     }
 ) => {
+    const onChangeCallback = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e);
+        onChangeText && onChangeText(e.currentTarget.value);
+    }, [onChange, onChangeText]);
 
     log(renderLog || `rendering Input`);
     return (
-        <Input className={`${s.base} ${className}`} {...restProps}/>
+        <Input onChange={onChangeCallback} className={`${s.base} ${className}`} {...restProps}/>
     );
 });
 
