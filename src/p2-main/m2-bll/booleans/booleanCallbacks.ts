2c247f1db93cb71b59587a283d551933684f652a
ai73aaa <1qazxcvBG>
2020-07-16 20:39:20 +0300

    add boolean callbacks

---
+++ src/p2-main/m2-bll/booleans/booleanCallbacks.ts
@@ -0,0 +1,33 @@
+import {Dispatch} from "redux";
+import {BooleanActions} from "./BooleanActions";
+
+// BOOLEAN_NAMES == [loading, error, success]
+
+export const setBooleanLoading = (dispatch: Dispatch, BOOLEAN_NAMES: string[], loading: boolean) => {
+    dispatch(BooleanActions.setBooleanValues([
+        {name: BOOLEAN_NAMES[0], value: loading},
+        {name: BOOLEAN_NAMES[1], value: false, data: undefined},
+        {name: BOOLEAN_NAMES[2], value: false}
+    ]))
+};
+export const setBooleanError = (dispatch: Dispatch, BOOLEAN_NAMES: string[], error: string) => {
+    dispatch(BooleanActions.setBooleanValues([
+        {name: BOOLEAN_NAMES[0], value: false},
+        {name: BOOLEAN_NAMES[1], value: true, data: error},
+        {name: BOOLEAN_NAMES[2], value: false},
+    ]))
+};
+export const setBooleanSuccess = (dispatch: Dispatch, BOOLEAN_NAMES: string[], success: boolean) => {
+    dispatch(BooleanActions.setBooleanValues([
+        {name: BOOLEAN_NAMES[0], value: false},
+        {name: BOOLEAN_NAMES[1], value: false, data: undefined},
+        {name: BOOLEAN_NAMES[2], value: success},
+    ]))
+};
+export const clearBooleans = (dispatch: Dispatch, BOOLEAN_NAMES: string[]) => {
+    dispatch(BooleanActions.setBooleanValues([
+        {name: BOOLEAN_NAMES[0], value: false},
+        {name: BOOLEAN_NAMES[1], value: false, data: undefined},
+        {name: BOOLEAN_NAMES[2], value: false},
+    ]))
+};
