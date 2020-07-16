d83829b59e1895848b8ab330e229351f5044b8db
ai73aaa <1qazxcvBG>
2020-07-16 18:48:20 +0300

    fix welcome story

--- src/stories/0-Welcome.stories.tsx
+++ src/stories/0-Welcome.stories.tsx
@@ -7,7 +7,7 @@ export default {
   component: Welcome,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo("CustomInput")} />;
+export const ToStorybook = () => <Welcome showApp={linkTo("S1_CustomButton")} />;
 
 ToStorybook.story = {
   name: "to Storybook",
