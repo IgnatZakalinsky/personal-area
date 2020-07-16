47a96e7995b9eed9467aaac1ac20be260743a8c5
ai73aaa <1qazxcvBG>
2020-07-16 21:20:21 +0300

    add thunk tryCatch

---
+++ src/p2-main/m2-bll/thunks.ts
@@ -0,0 +1,26 @@
+import {AppStoreType} from "./store";
+import {log} from "../../p1-common/c0-debug/debug";
+import {Dispatch} from "redux";
+import { setBooleanError } from "./booleans/booleanCallbacks";
+
+export type ReturnVoid = void;
+export type ExtraArgumentNya = {};
+export type GetAppStoreType = () => AppStoreType;
+
+export const tryCatch = async (
+    logic: () => void,
+    dispatch: Dispatch,
+    BOOLEAN_NAMES: string[],
+
+    info?: string,
+) => {
+    try {
+        await logic();
+
+    } catch (e) {
+        const error = e.response ? e.response.data.error : (e.message + ', more details in the console');
+        setBooleanError(dispatch, BOOLEAN_NAMES, error);
+
+        log(info + ' Error! :', {...e})
+    }
+};
