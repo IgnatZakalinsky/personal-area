1c4be3d0c8f07084eb577ae9165991df721b709e
ai73aaa <1qazxcvBG>
2020-07-16 21:21:10 +0300

    add login thunk

--- src/p3-features/f1-auth/a1-login/l1-ui/LoginForm/LoginFormContainer.tsx
+++ src/p3-features/f1-auth/a1-login/l1-ui/LoginForm/LoginFormContainer.tsx
@@ -2,14 +2,21 @@ import React, {useCallback, useState} from "react";
 import {log} from "../../../../../p1-common/c0-debug/debug";
 import LoginForm, {LoginFormDataType, LoginFormErrorDataType} from "./LoginForm";
 import {useBooleanSelector} from "../../../../../p2-main/m2-bll/booleans/useBooleanSelectors";
+import {useDispatch} from "react-redux";
+import {sendTokenTC} from "../../l2-bll/sendToken";
+
+const LOGIN_BOOLEAN_NAMES = ["LOGIN/LOADING", "LOGIN/ERROR", "LOGIN/SUCCESS"];
 
 const LoginFormContainer = React.memo(() => {
     // const [token, setToken] = useState<string>("");
     const [token, setToken] = useState<string>("b8d798e0-a1ae-11ea-b70e-e92253bbd4bd");
-    // const loading = true;
-    const [loading] = useBooleanSelector(["loading"]);
+    const [loading, error, success] = useBooleanSelector(LOGIN_BOOLEAN_NAMES);
 
-    const sendToken = useCallback(() => loading || alert(token), [loading, token]);
+    const dispatch = useDispatch();
+    const sendToken = useCallback(() => {
+        if (!loading.value) dispatch(sendTokenTC(token, LOGIN_BOOLEAN_NAMES))
+    }, [dispatch, loading, token]);
+
     const onFinish = useCallback((values: LoginFormDataType) => {
         log('Success:', values);
         sendToken();
@@ -19,6 +26,7 @@ const LoginFormContainer = React.memo(() => {
     }, []);
 
     log("4 ---- rendering LoginFormContainer");
+    log(error, success)
     return (
         <LoginForm
             loading={loading.value}
---
+++ src/p3-features/f1-auth/a1-login/l2-bll/sendToken.ts
@@ -0,0 +1,33 @@
+import {ExtraArgumentNya, ReturnVoid, tryCatch} from "../../../../p2-main/m2-bll/thunks";
+import {AppStoreType} from "../../../../p2-main/m2-bll/store";
+import {BooleanActionsType} from "../../../../p2-main/m2-bll/booleans/BooleanActions";
+import {ThunkAction} from "redux-thunk";
+import {setBooleanLoading, setBooleanSuccess} from "../../../../p2-main/m2-bll/booleans/booleanCallbacks";
+import {log} from "../../../../p1-common/c0-debug/debug";
+
+export const sendTokenTC =
+    (token: string, BOOLEAN_NAMES: string[])
+        : ThunkAction<ReturnVoid, AppStoreType, ExtraArgumentNya, BooleanActionsType> =>
+        async (
+            dispatch,
+            getStore
+        ) => {
+            setBooleanLoading(dispatch, BOOLEAN_NAMES, true);
+
+            await tryCatch(
+                async () => {
+                    const data = await new Promise(
+                        // (res, rej) => setTimeout(() => res("ok"), 3000)
+                        (res, rej) => setTimeout(() => rej({message: "error"}), 3000)
+                    );
+                        setBooleanSuccess(dispatch, BOOLEAN_NAMES, true);
+                        // dispatch(ProfileActions.setUser(data));
+
+                        log('Login with token Success!', data)
+
+                },
+                dispatch,
+                BOOLEAN_NAMES,
+                "Login with token"
+            );
+        };
