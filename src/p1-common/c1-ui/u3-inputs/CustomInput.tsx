2a0bc69ce262ec141aa35c6011659f2ec3788f17
ai73aaa <1qazxcvBG>
2020-07-19 19:42:16 +0300

    update class input

--- src/p1-common/c1-ui/u3-inputs/CustomInput.tsx
+++ src/p1-common/c1-ui/u3-inputs/CustomInput.tsx
@@ -35,17 +35,17 @@ export default CustomInput;
 
 // class input for fix antd form
 export class ClassCustomInput extends React.PureComponent<CustomInputPropsType> {
+    onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        this.props.onChange && this.props.onChange(e);
+        this.props.onChangeText && this.props.onChangeText(e.currentTarget.value);
+    };
+
     render() {
         const {onChangeText, onChange, renderLog, className, ...restProps} = this.props;
 
-        const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-            onChange && onChange(e);
-            onChangeText && onChangeText(e.currentTarget.value);
-        };
-
         log(renderLog || `rendering Input`);
         return (
-            <Input onChange={onChangeCallback} className={className || s.base} {...restProps}/>
+            <Input onChange={this.onChangeCallback} className={className || s.base} {...restProps}/>
         );
     }
 }
