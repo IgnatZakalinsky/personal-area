b9df9e9bdcf26ae23b45775a63ad195d8256fbe3
ai73aaa <1qazxcvBG>
2020-07-19 19:50:15 +0300

    show info about rendering

--- src/p1-common/c0-debug/debug.ts
+++ src/p1-common/c0-debug/debug.ts
@@ -3,3 +3,8 @@ import {IS_DEVELOPER_VERSION} from "../../p0-config/config";
 export const log = (...args: any[]) => {
     if (IS_DEVELOPER_VERSION) console.log(...args);
 };
+
+log(`
+!!! if component have useSomeHook - rendering 2 times
+
+`);
