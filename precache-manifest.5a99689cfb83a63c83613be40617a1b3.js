f852c13c49bdae55c7d1d254b65074582f223752
ai73aaa <1qazxcvBG>
2020-07-20 19:02:05 +0300

    Updates

---
+++ precache-manifest.5a99689cfb83a63c83613be40617a1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76af46f2e4a867a425ee9bb88d802cd0",
+    "url": "/personal-area/index.html"
+  },
+  {
+    "revision": "00ce5f45bf0009ff0484",
+    "url": "/personal-area/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "6f2d0dc37e001f8b2646",
+    "url": "/personal-area/static/css/main.e3b417a0.chunk.css"
+  },
+  {
+    "revision": "00ce5f45bf0009ff0484",
+    "url": "/personal-area/static/js/2.baee8290.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-area/static/js/2.baee8290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2d0dc37e001f8b2646",
+    "url": "/personal-area/static/js/main.647eee5a.chunk.js"
+  },
+  {
+    "revision": "bc3e94022b1cc53ac0fb",
+    "url": "/personal-area/static/js/runtime-main.f11a35dc.js"
+  }
+]);
